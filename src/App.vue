3c564e4fda4c47b4f3e6259c5ab80e5adb64f762
wenreq <294491328@qq.com>
2022-11-02 20:10:42 +0800

    feat(layout):项目布局开发完成

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,103 @@
 <script setup lang="ts">
 const ENV = import.meta.env;
-console.log(ENV)
+console.log(ENV);
 </script>
 
 <template>
-  <div>
-    <router-view></router-view>
+  <div class="layout">
+    <el-container class="container">
+      <el-aside class="aside">
+        <!--系统名称+logo-->
+        <div class="head">
+          <div>
+            <img src="//s.weituibao.com/1582958061265/mlogo.png" alt="logo" />
+            <span>vue3 admin</span>
+          </div>
+        </div>
+        <!--一条为了美观的线条-->
+        <div class="line" />
+        <el-menu background-color="#222832" text-color="#fff" :router="true">
+          <!--一级栏目-->
+          <el-sub-menu index="1">
+            <template #title>
+              <span>Dashboard</span>
+            </template>
+            <!--二级栏目-->
+            <el-menu-item-group>
+              <el-menu-item index="/"
+                ><el-icon><DataLine /></el-icon>首页</el-menu-item
+              >
+              <el-menu-item index="/add"><el-icon><DataLine /></el-icon>添加商品</el-menu-item>
+            </el-menu-item-group>
+          </el-sub-menu>
+        </el-menu>
+      </el-aside>
+      <!--右边内容布局-->
+      <el-container class="content">
+        <Header />
+        <div class="main">
+          <!--将 <router-view></router-view> 移到这里，并且用单标签-->
+          <router-view />
+        </div>
+        <Footer />
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <style scoped>
+.layout {
+  min-height: 100vh;
+  background-color: #ffffff;
+}
+.container {
+  height: 100vh;
+}
+.aside {
+  width: 200px !important;
+  background-color: #222832;
+}
+.content {
+  display: flex;
+  flex-direction: column;
+  max-height: 100vh;
+  overflow: hidden;
+}
+.main {
+  height: calc(100vh - 100px);
+  overflow: auto;
+  padding: 10px;
+}
+.head {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+}
+.head > div {
+  display: flex;
+  align-items: center;
+}
+
+.head img {
+  width: 50px;
+  height: 50px;
+  margin-right: 10px;
+}
+.head span {
+  font-size: 20px;
+  color: #ffffff;
+}
+.line {
+  border-top: 1px solid hsla(0, 0%, 100%, 0.05);
+  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+}
+</style>
+
+<style>
+body {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,18 @@ import {
   createWebHashHistory
 } from 'vue-router'
 import Index from '@/views/Index.vue'
+import AddGoodVue from '@/views/AddGood.vue'
 
 const router = createRouter({
   history: createWebHashHistory(), // hash 模式
   routes: [{
     path: '/',
+    name: 'index',
     component: Index
+  }, {
+    path: '/add',
+    name: 'add',
+    component: AddGoodVue
   }]
 })
 
